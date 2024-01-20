0df4f7a9101f1c6a46ce2be3ad77380a557b4073
Key Vo <bigdaddylover86@gmail.com>
2024-01-20 08:52:54 -0600

    Update account class

--- PHP&MySql/c04/class/Account.php
+++ PHP&MySql/c04/class/Account.php
@@ -1,16 +1,18 @@
 <?php
-    declare(strict_types = 1);
+    declare(strict_types=1);
+
     class Account {
         public int $number;
         public string $type;
         protected float $balance;
 
-        public function __contruct(int $number, string $type, float $balance = 0.00) {
+        public function __construct(int $number, string $type, float $balance = 0.00){
             $this->number = $number;
-            $this->type = $type;        
-            $this->balance = $balance;        
+            $this->type = $type;
+            $this->balance = $balance;
         }
-        public function deposit(float $amount) {
+
+        public function deposit(float $amount): float {
             $this->balance += $amount;
             return $this->balance;
         }
@@ -18,16 +20,12 @@
         public function withdraw(float $amount): float {
             $this->balance -= $amount;
             return $this->balance;
-        } 
+        }
 
-        public function getBalance(float $amount): float {
+        public function getBalance(): float {
             return $this->balance;
         }
+        
     }
 
-    $account = new Account(20148896, 'Savings', 80.00);
 ?>
-
-<h2><?= $account->type ?> Account</h2>
-<p>Previous balance: $<?= $account->getBalance() ?></p>
-<p>New balance: $<?= $account->deposit(35.00) ?></p>
